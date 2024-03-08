303372361ae529699a92a5d0cba34f859918af5e
Damar2121 <damar.vivoy51@gmail.com>
2024-03-08 11:10:27 +0700

    masukan data

--- 110_BubbleSort/110_BubbleSort.cpp
+++ 110_BubbleSort/110_BubbleSort.cpp
@@ -17,7 +17,17 @@ void input() {
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
     }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> a[i];
+    }
 }
+
 
 
 
